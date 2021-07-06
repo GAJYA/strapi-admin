a03d2de77a5f242dc2c7226deaa622653a58f340
luoli_bcc <luoli_bcc@si-tech.com.cn>
2021-07-06 17:59:23 +0800

    Create Strapi starter project

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '5d8933dda5bbf393736638918a15d79c'),
+    },
+  },
+});
